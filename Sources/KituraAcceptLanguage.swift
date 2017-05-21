7dc597110e40dd97eba78faa11d6e75c7aaf1b4d
Nocturnal <contact@nocturnal.solutions>
2017-05-20 18:26:58 -0600

    Add code and tests.

--- Sources/KituraAcceptLanguage.swift
+++ Sources/KituraAcceptLanguage.swift
@@ -1,4 +1,116 @@
-struct KituraAcceptLanguage {
+import Foundation
+import Kitura
+import LoggerAPI
 
-    var text = "Hello, World!"
+public struct LanguageQualityPair {
+    let langcode : String
+    let matchQuality : Float
+
+    init (negotiatedLanguage: String, matchQuality: Float = 0) {
+        self.langcode = negotiatedLanguage
+        self.matchQuality = matchQuality
+    }
+}
+
+public class AcceptLanguage : RouterMiddleware {
+
+    enum NegotiationStatus {
+        case NegotiationIncomplete, NoAcceptLanguageHeader, NoMatch, Matched
+    }
+
+    var negotiationStatus : NegotiationStatus = .NegotiationIncomplete
+    var negotiatedLanguage : LanguageQualityPair?
+
+    let sendContentLanguageHeader: Bool
+    let langsToAccept: [String]
+
+    public init(acceptLangs: [String], sendContentLanguageHeader: Bool = true) {
+        self.sendContentLanguageHeader = sendContentLanguageHeader
+        self.langsToAccept = acceptLangs
+    }
+
+    public func handle(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) {
+        defer {
+            next()
+        }
+
+        guard langsToAccept.count > 0 else {
+            Log.error("Language negotiation attempted with no accepted languages defined; match impossible")
+            negotiationStatus = .NoMatch
+            return
+        }
+
+        guard let acceptHeader = request.headers["Accept-Language"] else {
+            negotiationStatus = .NoAcceptLanguageHeader
+            negotiatedLanguage = LanguageQualityPair(negotiatedLanguage: langsToAccept.first!)
+            return
+        }
+
+        // Mad props to http://nshipster.com/nsregularexpression/ for helping me
+        // navigate the ridiculousness that is RegEx in Swift.
+        let pattern = try! NSRegularExpression(pattern: "([a-zA-Z-]+|\\*)(?:.+?([\\d\\.]+))?")
+
+        var currentBestMatch: LanguageQualityPair?
+
+        for acceptable in acceptHeader.components(separatedBy: ",") {
+            let acceptableCount = acceptable.utf16.count
+            guard let match = pattern.firstMatch(in: acceptable, options: [], range: NSRange(location: 0, length: acceptableCount)) else {
+                // This code seems to be seriously malformed.
+                continue
+            }
+
+            // Extract the langcode
+            let langcodeRange = match.rangeAt(1)
+            let start = String.UTF16Index(langcodeRange.location)
+            let end = String.UTF16Index(langcodeRange.location + langcodeRange.length)
+            var langcode = String(acceptable.utf16[start..<end])!
+            if langcode == "*" {
+                langcode = langsToAccept.first!
+            }
+
+            let quality: Float?
+            let qualityRange = match.rangeAt(2)
+            if qualityRange.location == NSNotFound {
+                // If there's no quality specified, use 1.
+                quality = 1.0
+            }
+            else {
+                // Extract the quality.
+                let start = String.UTF16Index(qualityRange.location)
+                let end = String.UTF16Index(qualityRange.location + qualityRange.length)
+                quality = Float(String(acceptable.utf16[start..<end])!)
+                // "all languages which are assigned a quality factor greater
+                // than 0 are acceptable," so treat a best match with a quality
+                // of 0 as no match at all. (If quality was so malformed that we
+                // couldn't parse it, treat that as zero.)
+                if quality == nil || quality == 0 {
+                    continue;
+                }
+            }
+
+            if langsToAccept.index(of: langcode) != nil && (currentBestMatch == nil || quality! > currentBestMatch!.matchQuality) {
+                currentBestMatch = LanguageQualityPair(negotiatedLanguage: langcode, matchQuality: quality!)
+                if quality! == 1.0 {
+                    // We can't do better than this, so stop iterating.
+                    break
+                }
+            }
+        }
+
+        // "all languages which are assigned a quality factor greater than 0 are
+        // acceptable," so treat a best match with a quality of 0 as no match at
+        // all.
+        if currentBestMatch == nil {
+            negotiationStatus = .NoMatch
+            negotiatedLanguage = LanguageQualityPair(negotiatedLanguage: langsToAccept.first!, matchQuality: 0)
+        }
+        else {
+            negotiationStatus = .Matched
+            negotiatedLanguage = currentBestMatch
+        }
+
+        if sendContentLanguageHeader {
+            response.headers.append("Content-Language", value: negotiatedLanguage!.langcode)
+        }
+    }
 }
--- Tests/KituraAcceptLanguageTests/KituraAcceptLanguageTests.swift
+++ Tests/KituraAcceptLanguageTests/KituraAcceptLanguageTests.swift
@@ -1,15 +1,167 @@
 import XCTest
+import Kitura
+import KituraNet
 @testable import KituraAcceptLanguage
 
 class KituraAcceptLanguageTests: XCTestCase {
+
+    let router = KituraAcceptLanguageTests.setupRouter()
+
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(KituraAcceptLanguage().text, "Hello, World!")
+//        XCTAssertEqual(KituraAcceptLanguage().text, "Hello, World!")
     }
 
+    func testNoReqHeader() {
+        performServerTest(router: router) { expectation in
+            self.performGetWithAcceptLanguage("/accept-lang", acceptLanguage: nil, callback: { response in
+                XCTAssertEqual(AcceptLanguage.NegotiationStatus.NoAcceptLanguageHeader.hashValue, Int(response!.headers["X-Negotiation-Status"]!.first!), "Mishandled missing Accept-Language header")
+                expectation.fulfill()
+            })
+        }
+    }
+
+    func testSimpleNegotiation() {
+        performServerTest(router: router) { expectation in
+            self.performGetWithAcceptLanguage("/accept-lang", acceptLanguage: "no, ja", callback: { response in
+                XCTAssertEqual(response!.headers["X-Langcode"]!.first!, "ja", "Did not negotiate match")
+                XCTAssertNotNil(response!.headers["Content-Language"], "Did not add Content-Language header")
+                if let clHeader = response!.headers["Content-Language"] {
+                    XCTAssertEqual(clHeader.first!, "ja", "Did not properly set Content-Language header")
+                }
+            })
+            self.performGetWithAcceptLanguage("/accept-lang", acceptLanguage: "no, fi, sv", callback: { response in
+                XCTAssertEqual(AcceptLanguage.NegotiationStatus.NoMatch.hashValue, Int(response!.headers["X-Negotiation-Status"]!.first!), "Did not recognize failure to match")
+                XCTAssertEqual(response!.headers["X-Langcode"]!.first!, "en", "Did not fall back properly on no match")
+            })
+            expectation.fulfill()
+        }
+    }
+
+    func testNegotiationWithQuality() {
+        performServerTest(router: router) { expectation in
+            self.performGetWithAcceptLanguage("/accept-lang", acceptLanguage: "zh-hans, ja;q=0.9, en;q=0.8", callback: { response in
+                XCTAssertEqual(response!.headers["X-Langcode"]!.first!, "ja", "Did not negotiate match factoring quality scores properly")
+                XCTAssertEqual(response!.headers["X-Match-Quality"]!.first!, "0.9", "Did not calculate match quality correctly")
+                
+            })
+            expectation.fulfill()
+        }
+    }
+
+    func testNoResHeader() {
+        performServerTest(router: router) { expectation in
+            self.performGetWithAcceptLanguage("/accept-lang-no-header", acceptLanguage: "zh-hans", callback: { response in
+                XCTAssertEqual(response!.headers["X-Langcode"]!.first!, "zh-hans", "Did not negotiate match when not sending Content-Language header")
+                XCTAssertNil(response!.headers["Content-Language"], "Content-Langauge header sent when it should not have been")
+            })
+            expectation.fulfill()
+        }
+    }
+
+    func testOddities() {
+        performServerTest(router: router) { expectation in
+            self.performGetWithAcceptLanguage("/accept-lang-failure", acceptLanguage: "ja", callback: { response in
+                XCTAssertEqual(Int(response!.headers["X-Negotiation-Status"]!.first!), AcceptLanguage.NegotiationStatus.NoMatch.hashValue, "Impossible match")
+            })
+            self.performGetWithAcceptLanguage("/accept-lang", acceptLanguage: "qwert;1=, q=yiop, ;;;,,, asdf;q=4, en;q=0, ja, ..zxcv", callback: { response in
+                guard response!.headers["X-Langcode"] != nil else {
+                    XCTFail("Failed to make a match through garbage in the Accept-Language header")
+                    return
+                }
+                XCTAssertEqual(response!.headers["X-Langcode"]!.first!, "ja", "Failed to make a match through garbage in the Accept-Language header")
+            })
+            expectation.fulfill()
+        }}
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testNoReqHeader", testNoReqHeader),
+        ("testSimpleNegotiation", testSimpleNegotiation),
+        ("testNegotiationWithQuality", testNegotiationWithQuality),
+        ("testNoResHeader", testNoResHeader),
+        ("testOddities", testOddities),
     ]
+
+    static func setupRouter() -> Router {
+        let router = Router()
+
+        let routes = [
+            "/accept-lang": AcceptLanguage(acceptLangs: ["en", "ja"]),
+            "/accept-lang-failure": AcceptLanguage(acceptLangs: []),
+            "/accept-lang-no-header": AcceptLanguage(acceptLangs: ["en", "zh-hans"], sendContentLanguageHeader: false)
+        ]
+
+        for (path, al) in routes {
+            router.get(path, middleware: al)
+            router.get(path) { _, response, next in
+                response.headers["X-Negotiation-Status"] = String(al.negotiationStatus.hashValue)
+                if let lang = al.negotiatedLanguage {
+                    response.headers["X-Langcode"] = lang.langcode
+                    response.headers["X-Match-Quality"] = String(lang.matchQuality)
+
+                }
+                next()
+            }
+        }
+        return router
+    }
+
+    // Ripped off from the Kitura-CredentialsHTTP tests.
+    func performServerTest(router: ServerDelegate, asyncTasks: @escaping (XCTestExpectation) -> Void...) {
+        do {
+            let server = try HTTPServer.listen(on: 8090, delegate: router)
+            let requestQueue = DispatchQueue(label: "Request queue")
+
+            for (index, asyncTask) in asyncTasks.enumerated() {
+                let expectation = self.expectation(index)
+                requestQueue.async {
+                    asyncTask(expectation)
+                }
+            }
+
+            waitExpectation(timeout: 10) { error in
+                // blocks test until request completes
+                server.stop()
+                XCTAssertNil(error);
+            }
+        } catch {
+            XCTFail("Error: \(error)")
+        }
+    }
+
+    func performRequest(method: String, host: String = "localhost", path: String, callback: @escaping ClientRequest.Callback, headers: [String: String]? = nil, requestModifier: ((ClientRequest) -> Void)? = nil) {
+        var allHeaders = [String: String]()
+        if  let headers = headers  {
+            for  (headerName, headerValue) in headers  {
+                allHeaders[headerName] = headerValue
+            }
+        }
+        allHeaders["Content-Type"] = "text/plain"
+        let options: [ClientRequest.Options] =
+            [.method(method), .hostname(host), .port(8090), .path(path), .headers(allHeaders)]
+        let req = HTTP.request(options, callback: callback)
+        if let requestModifier = requestModifier {
+            requestModifier(req)
+        }
+        req.end()
+    }
+
+    func expectation(_ index: Int) -> XCTestExpectation {
+        let expectationDescription = "\(type(of: self))-\(index)"
+        return self.expectation(description: expectationDescription)
+    }
+
+    func waitExpectation(timeout t: TimeInterval, handler: XCWaitCompletionHandler?) {
+        self.waitForExpectations(timeout: t, handler: handler)
+    }
+
+    func performGetWithAcceptLanguage(_ path: String, acceptLanguage: String?, callback: @escaping ClientRequest.Callback) {
+        var headers: [String : String] = [:]
+        if let acceptLanguage = acceptLanguage {
+            headers["Accept-Language"] = acceptLanguage
+        }
+        performRequest(method: "get", host: "localhost", path: path, callback: callback, headers: headers, requestModifier: nil)
+    }
+
 }
