414fbe8f064f50b9b048d23c3c2d39e64ea727cb
Nocturnal <contact@nocturnal.solutions>
2017-06-16 12:17:59 -0600

    Be less picky about the HeliumLogger version we use.

--- Package.swift
+++ Package.swift
@@ -6,6 +6,6 @@ let package = Package(
     name: "KituraAcceptLanguage",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 7),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1)
     ]
 )
